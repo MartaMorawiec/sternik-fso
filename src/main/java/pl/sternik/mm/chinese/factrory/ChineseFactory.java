7a2952238c76e45172656b75049d261be26b2e13
Marta Morawiec <marta.morawiec@edu.uekat.pl>
2018-03-04 14:41:41 +0100

    chinese factory

--- src/main/java/pl/sternik/mm/chinese/factrory/ChineseFactory.java
+++ src/main/java/pl/sternik/mm/chinese/factrory/ChineseFactory.java
@@ -47,15 +47,16 @@ public class ChineseFactory {
         factory.setProductionLine(new RaceCar());
         for (int i = 1; i < 5; i++)
             System.out.println(factory.produceNextItem());
+
         factory.setProductionLine(new Bear());
         for (int i = 1; i < 5; i++)
             System.out.println(factory.produceNextItem());
 
-        System.out.println("-----------------------");
+        System.out.println("----------++++-------------");
         long start = System.nanoTime();
         CarBuilder kabrioBuilder = new KabrioBuilder();
         CarBuildDirector carBuildDirector = new CarBuildDirector(kabrioBuilder);
-        
+
         Car car = carBuildDirector.constructSportVersion();
         System.out.println("Produkcja pierwszego cara:" + (System.nanoTime() - start));
         start = System.nanoTime();
@@ -70,7 +71,7 @@ public class ChineseFactory {
             produkcja[i] = factory.produceNextItem();
         long duration = System.nanoTime() - start;
         System.out.println("Klon 5 carów:" + duration + " srednio na jeden:" + duration / 5);
-        
+
         for (int i = 0; i < produkcja.length; i++) {
             System.out.println(produkcja[i]);
         }
